38765ff127519311d39ad822ff6bf1d7294f3481
Alpays <73616321+Alpays@users.noreply.github.com>
2021-01-10 17:50:48 +0300

    Fixed warnings

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,44 +1,45 @@
 #include <iostream>
 #include "Chip-8.h"
-#include "SDL.h"
 #include <chrono>
 #include "SdlClass.h"
 #include <Windows.h> // for FreeConsole function.
 
 
 int main(int argc, char *argv[]) {
-	SdlClass platform("Alpay's Chip-8 Emulator (AC-8)", VIDEO_WIDTH * 10, VIDEO_HEIGHT * 10, VIDEO_WIDTH, VIDEO_HEIGHT);
-	std::cout << "Type the file name you wish to load" << std::endl;
-	Chip8 chip8;
+	SdlClass* platform = new SdlClass("Alpay's Chip-8 Emulator (AC-8)", VIDEO_WIDTH * 10, VIDEO_HEIGHT * 10, VIDEO_WIDTH, VIDEO_HEIGHT);
+	Chip8* chip8 = new Chip8;
 	ASKGAME:
-	char gameName[32];
-	std::cin.getline(gameName, 32);
-	if (!chip8.loadRom(gameName))
+	std::cout << "Type the file name you wish to load" << std::endl;
+	char gameName[64];
+	std::cin.getline(gameName, 64);
+	if (!chip8->loadRom(gameName))
 	{
 		std::cout << "Cannot find the game" << std::endl;
+		Sleep(3000);
 		goto ASKGAME;
 	}
-	else FreeConsole();
-	int videoPitch = sizeof(chip8.video[0]) * VIDEO_WIDTH;
+	else FreeConsole(); chip8->paused = false;
+	int videoPitch = sizeof(chip8->video[0]) * VIDEO_WIDTH;
 
 	auto lastCycleTime = std::chrono::high_resolution_clock::now();
 	bool quit = false;
 
 	while (!quit)
 	{
-		quit = platform.Input(chip8.keypad);
+		quit = platform->Input(chip8->keypad, chip8);
 
 		auto currentTime = std::chrono::high_resolution_clock::now();
 		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
-
 		if (dt > 5) // Decrease the number to make games faster
 		{
 			lastCycleTime = currentTime;
 
-			chip8.cycles();
+			chip8->cycles();
 
-			platform.Update(chip8.video, videoPitch);
+			platform->Update(chip8->video, videoPitch);
 		}
 	}
+	delete platform;
+	delete chip8;
 	return 0;
 }
