a268566fefa2f0dff45298350e15301b11648d6a
Alpays <73616321+Alpays@users.noreply.github.com>
2021-01-10 17:53:34 +0300

    Fixed warnings

--- source/SdlClass.h
+++ source/SdlClass.h
@@ -2,10 +2,12 @@
 
 
 #include <cstdint>
+#include "Chip-8.h"
 
-class SDL_Window;
-class SDL_Renderer;
-class SDL_Texture;
+
+struct SDL_Window;
+struct SDL_Renderer;
+struct SDL_Texture;
 
 class SdlClass
 {
@@ -13,7 +15,7 @@ public:
 	SdlClass(const char* title, int width, int height, int textureW, int textureH);
 	~SdlClass();
 	void Update(const void* buffer, int pitch);
-	bool Input(uint8_t* keys);
+	bool Input(uint8_t* keys, Chip8* ch8 = nullptr);
 private:
 	SDL_Window* window{};
 	SDL_Renderer* renderer{};
